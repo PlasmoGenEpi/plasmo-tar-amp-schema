0c4dfbe3a8d710fb399833ede12757dfd25cd97d
Nicholas Hathaway <nickjhathaway@gmail.com>
2023-08-07 14:42:35 -0400

    chore: initial commit

--- project/sqlschema/plasmo_tar_amp_schema.sql
+++ project/sqlschema/plasmo_tar_amp_schema.sql
@@ -9,9 +9,31 @@ CREATE TABLE "GenomeInfo" (
 	PRIMARY KEY (name, version, taxon_id, url, gff_url)
 );
 
+CREATE TABLE "GenomicLocation" (
+	chrom TEXT NOT NULL, 
+	start INTEGER NOT NULL, 
+	"end" INTEGER NOT NULL, 
+	strand TEXT, 
+	PRIMARY KEY (chrom, start, "end", strand)
+);
+
+CREATE TABLE "PrimerInfo" (
+	seq TEXT NOT NULL, 
+	location TEXT NOT NULL, 
+	PRIMARY KEY (seq, location)
+);
+
+CREATE TABLE "Primers" (
+	entries TEXT, 
+	PRIMARY KEY (entries)
+);
+
 CREATE TABLE "TargetInfo" (
 	target_id TEXT NOT NULL, 
 	gene_id TEXT, 
 	target_genome TEXT NOT NULL, 
-	PRIMARY KEY (target_id, gene_id, target_genome)
+	insert_location TEXT NOT NULL, 
+	forward_primers TEXT NOT NULL, 
+	reverse_primers TEXT NOT NULL, 
+	PRIMARY KEY (target_id, gene_id, target_genome, insert_location, forward_primers, reverse_primers)
 );
--- src/plasmo_tar_amp_schema/datamodel/plasmo_tar_amp_schema.py
+++ src/plasmo_tar_amp_schema/datamodel/plasmo_tar_amp_schema.py
@@ -1,5 +1,5 @@
 # Auto generated from plasmo_tar_amp_schema.yaml by pythongen.py version: 0.9.0
-# Generation date: 2023-08-07T14:28:12
+# Generation date: 2023-08-07T14:42:28
 # Schema: plasmo-tar-amp-schema
 #
 # id: https://plasmogenepi.github.io/plasmo-tar-amp-schema
@@ -59,6 +59,9 @@ class TargetInfo(YAMLRoot):
 
     target_id: str = None
     target_genome: Union[dict, "GenomeInfo"] = None
+    insert_location: Union[dict, "GenomicLocation"] = None
+    forward_primers: Union[dict, "Primers"] = None
+    reverse_primers: Union[dict, "Primers"] = None
     gene_id: Optional[str] = None
 
     def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
@@ -71,6 +74,21 @@ class TargetInfo(YAMLRoot):
             self.MissingRequiredField("target_genome")
         if not isinstance(self.target_genome, GenomeInfo):
             self.target_genome = GenomeInfo(**as_dict(self.target_genome))
+
+        if self._is_empty(self.insert_location):
+            self.MissingRequiredField("insert_location")
+        if not isinstance(self.insert_location, GenomicLocation):
+            self.insert_location = GenomicLocation(**as_dict(self.insert_location))
+
+        if self._is_empty(self.forward_primers):
+            self.MissingRequiredField("forward_primers")
+        if not isinstance(self.forward_primers, Primers):
+            self.forward_primers = Primers(**as_dict(self.forward_primers))
+
+        if self._is_empty(self.reverse_primers):
+            self.MissingRequiredField("reverse_primers")
+        if not isinstance(self.reverse_primers, Primers):
+            self.reverse_primers = Primers(**as_dict(self.reverse_primers))
 
         if self.gene_id is not None and not isinstance(self.gene_id, str):
             self.gene_id = str(self.gene_id)
@@ -123,6 +141,94 @@ class GenomeInfo(YAMLRoot):
         super().__post_init__(**kwargs)
 
 
+@dataclass
+class GenomicLocation(YAMLRoot):
+    """
+    information on the genomic location of specific sequence
+    """
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = PLASMO_TAR_AMP_SCHEMA.GenomicLocation
+    class_class_curie: ClassVar[str] = "plasmo_tar_amp_schema:GenomicLocation"
+    class_name: ClassVar[str] = "GenomicLocation"
+    class_model_uri: ClassVar[URIRef] = PLASMO_TAR_AMP_SCHEMA.GenomicLocation
+
+    chrom: str = None
+    start: int = None
+    end: int = None
+    strand: Optional[str] = None
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        if self._is_empty(self.chrom):
+            self.MissingRequiredField("chrom")
+        if not isinstance(self.chrom, str):
+            self.chrom = str(self.chrom)
+
+        if self._is_empty(self.start):
+            self.MissingRequiredField("start")
+        if not isinstance(self.start, int):
+            self.start = int(self.start)
+
+        if self._is_empty(self.end):
+            self.MissingRequiredField("end")
+        if not isinstance(self.end, int):
+            self.end = int(self.end)
+
+        if self.strand is not None and not isinstance(self.strand, str):
+            self.strand = str(self.strand)
+
+        super().__post_init__(**kwargs)
+
+
+@dataclass
+class PrimerInfo(YAMLRoot):
+    """
+    information on a primer sequence
+    """
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = PLASMO_TAR_AMP_SCHEMA.PrimerInfo
+    class_class_curie: ClassVar[str] = "plasmo_tar_amp_schema:PrimerInfo"
+    class_name: ClassVar[str] = "PrimerInfo"
+    class_model_uri: ClassVar[URIRef] = PLASMO_TAR_AMP_SCHEMA.PrimerInfo
+
+    seq: str = None
+    location: Union[dict, GenomicLocation] = None
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        if self._is_empty(self.seq):
+            self.MissingRequiredField("seq")
+        if not isinstance(self.seq, str):
+            self.seq = str(self.seq)
+
+        if self._is_empty(self.location):
+            self.MissingRequiredField("location")
+        if not isinstance(self.location, GenomicLocation):
+            self.location = GenomicLocation(**as_dict(self.location))
+
+        super().__post_init__(**kwargs)
+
+
+@dataclass
+class Primers(YAMLRoot):
+    """
+    A holder of primer sequences
+    """
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = PLASMO_TAR_AMP_SCHEMA.Primers
+    class_class_curie: ClassVar[str] = "plasmo_tar_amp_schema:Primers"
+    class_name: ClassVar[str] = "Primers"
+    class_model_uri: ClassVar[URIRef] = PLASMO_TAR_AMP_SCHEMA.Primers
+
+    entries: Optional[Union[Union[dict, PrimerInfo], List[Union[dict, PrimerInfo]]]] = empty_list()
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        self._normalize_inlined_as_dict(slot_name="entries", slot_type=PrimerInfo, key_name="seq", keyed=False)
+
+        super().__post_init__(**kwargs)
+
+
 # Enumerations
 
 
@@ -140,6 +246,15 @@ slots.targetInfo__gene_id = Slot(uri=PLASMO_TAR_AMP_SCHEMA.gene_id, name="target
 
 slots.targetInfo__target_genome = Slot(uri=PLASMO_TAR_AMP_SCHEMA.target_genome, name="targetInfo__target_genome", curie=PLASMO_TAR_AMP_SCHEMA.curie('target_genome'),
                    model_uri=PLASMO_TAR_AMP_SCHEMA.targetInfo__target_genome, domain=None, range=Union[dict, GenomeInfo])
+
+slots.targetInfo__insert_location = Slot(uri=PLASMO_TAR_AMP_SCHEMA.insert_location, name="targetInfo__insert_location", curie=PLASMO_TAR_AMP_SCHEMA.curie('insert_location'),
+                   model_uri=PLASMO_TAR_AMP_SCHEMA.targetInfo__insert_location, domain=None, range=Union[dict, GenomicLocation])
+
+slots.targetInfo__forward_primers = Slot(uri=PLASMO_TAR_AMP_SCHEMA.forward_primers, name="targetInfo__forward_primers", curie=PLASMO_TAR_AMP_SCHEMA.curie('forward_primers'),
+                   model_uri=PLASMO_TAR_AMP_SCHEMA.targetInfo__forward_primers, domain=None, range=Union[dict, Primers])
+
+slots.targetInfo__reverse_primers = Slot(uri=PLASMO_TAR_AMP_SCHEMA.reverse_primers, name="targetInfo__reverse_primers", curie=PLASMO_TAR_AMP_SCHEMA.curie('reverse_primers'),
+                   model_uri=PLASMO_TAR_AMP_SCHEMA.targetInfo__reverse_primers, domain=None, range=Union[dict, Primers])
 
 slots.genomeInfo__name = Slot(uri=PLASMO_TAR_AMP_SCHEMA.name, name="genomeInfo__name", curie=PLASMO_TAR_AMP_SCHEMA.curie('name'),
                    model_uri=PLASMO_TAR_AMP_SCHEMA.genomeInfo__name, domain=None, range=str,
@@ -160,3 +275,29 @@ slots.genomeInfo__url = Slot(uri=PLASMO_TAR_AMP_SCHEMA.url, name="genomeInfo__ur
 slots.genomeInfo__gff_url = Slot(uri=PLASMO_TAR_AMP_SCHEMA.gff_url, name="genomeInfo__gff_url", curie=PLASMO_TAR_AMP_SCHEMA.curie('gff_url'),
                    model_uri=PLASMO_TAR_AMP_SCHEMA.genomeInfo__gff_url, domain=None, range=Optional[str],
                    pattern=re.compile(r'r"^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$"'))
+
+slots.genomicLocation__chrom = Slot(uri=PLASMO_TAR_AMP_SCHEMA.chrom, name="genomicLocation__chrom", curie=PLASMO_TAR_AMP_SCHEMA.curie('chrom'),
+                   model_uri=PLASMO_TAR_AMP_SCHEMA.genomicLocation__chrom, domain=None, range=str,
+                   pattern=re.compile(r'^[A-z-._0-9]$'))
+
+slots.genomicLocation__start = Slot(uri=PLASMO_TAR_AMP_SCHEMA.start, name="genomicLocation__start", curie=PLASMO_TAR_AMP_SCHEMA.curie('start'),
+                   model_uri=PLASMO_TAR_AMP_SCHEMA.genomicLocation__start, domain=None, range=int,
+                   pattern=re.compile(r'^[0-9]$'))
+
+slots.genomicLocation__end = Slot(uri=PLASMO_TAR_AMP_SCHEMA.end, name="genomicLocation__end", curie=PLASMO_TAR_AMP_SCHEMA.curie('end'),
+                   model_uri=PLASMO_TAR_AMP_SCHEMA.genomicLocation__end, domain=None, range=int,
+                   pattern=re.compile(r'^[0-9]$'))
+
+slots.genomicLocation__strand = Slot(uri=PLASMO_TAR_AMP_SCHEMA.strand, name="genomicLocation__strand", curie=PLASMO_TAR_AMP_SCHEMA.curie('strand'),
+                   model_uri=PLASMO_TAR_AMP_SCHEMA.genomicLocation__strand, domain=None, range=Optional[str],
+                   pattern=re.compile(r'r'[+-]''))
+
+slots.primerInfo__seq = Slot(uri=PLASMO_TAR_AMP_SCHEMA.seq, name="primerInfo__seq", curie=PLASMO_TAR_AMP_SCHEMA.curie('seq'),
+                   model_uri=PLASMO_TAR_AMP_SCHEMA.primerInfo__seq, domain=None, range=str,
+                   pattern=re.compile(r'^[A-z-._0-9]$'))
+
+slots.primerInfo__location = Slot(uri=PLASMO_TAR_AMP_SCHEMA.location, name="primerInfo__location", curie=PLASMO_TAR_AMP_SCHEMA.curie('location'),
+                   model_uri=PLASMO_TAR_AMP_SCHEMA.primerInfo__location, domain=None, range=Union[dict, GenomicLocation])
+
+slots.primers__entries = Slot(uri=PLASMO_TAR_AMP_SCHEMA.entries, name="primers__entries", curie=PLASMO_TAR_AMP_SCHEMA.curie('entries'),
+                   model_uri=PLASMO_TAR_AMP_SCHEMA.primers__entries, domain=None, range=Optional[Union[Union[dict, PrimerInfo], List[Union[dict, PrimerInfo]]]])
